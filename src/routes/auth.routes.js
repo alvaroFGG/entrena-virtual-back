994f03ab4f434fbb8905fb93eec7ae1947d16b2a
Álvaro Fuentenebro <alvarofuentenebrogomez@gmail.com>
2024-07-15 14:57:38 +0200

    feat: params/body schema validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { login } from "../controllers/auth.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", validateSchema(login), login);
 
 export default router;
