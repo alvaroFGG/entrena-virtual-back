bcb8aa95aee9b380c6c348d730ee4ca35d50cedb
Álvaro Fuentenebro <alvarofuentenebrogomez@gmail.com>
2024-07-15 16:21:23 +0200

    fix: validate schema

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { login } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
+import { loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.post("/login", validateSchema(login), login);
+router.post("/login", validateSchema(loginSchema), login);
 
 export default router;
